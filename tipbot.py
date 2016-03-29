08be338f0768f61a47bc06875085d63bde0ab417
Charles <cryptocharles@gmail.com>
2016-03-29 20:00:48 +0200

    fix json responses

--- tipbot.py
+++ tipbot.py
@@ -8,15 +8,15 @@ from two1.lib.bitrequests import BitTransferRequests
 wallet = Wallet()
 username = Config().username
 requests = BitTransferRequests(wallet, username)
-server_url = 'http://10.244.195.170:7777/tipbot/tips/'
+server_url = 'http://10.244.195.170:7777/tipbot/'
 
 @click.command()
 @click.argument('username')
 @click.argument('amount')
 def cli(username, amount):
-    sel_url = server_url+'{}?amount={}'.format(username, amount)
-    click.echo(json.loads(requests.get(url=sel_url)))
+    sel_url = server_url+'tips/{}?amount={}'.format(username, amount)
+    click.echo(json.loads(requests.get(url=sel_url).text))
 
 @click.command()
 def last():
-    click.echo(json.loads(requests.get(url=server_url+'/last-tips')))
+    click.echo(json.loads(requests.get(url=server_url+'last-tips').text))
