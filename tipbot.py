78f8f7a341c7a947b9609b867affac4ddb995d20
cryptocharles <cryptocharles@gmail.com>
2016-11-06 11:35:11 +0000

    ipv6

--- tipbot.py
+++ tipbot.py
@@ -8,14 +8,15 @@ from two1.bitrequests import BitTransferRequests
 wallet = Wallet()
 username = Config().username
 requests = BitTransferRequests(wallet, username)
-server_url = 'http://10.244.220.64:7773/tipbot/'
+server_url = 'http://[fcce:a977:ee73:e2bf:1b66:0000:0000:0001]:7773/tipbot/'
 
 @click.command()
 @click.argument('username')
 @click.argument('amount')
 def cli(username, amount):
     sel_url = server_url+'tips/{}?amount={}'.format(username, amount)
-    click.echo(json.loads(requests.get(url=sel_url).text))
+    text = requests.get(url=sel_url).text
+    click.echo(json.loads(text))
 
 @click.command()
 def last():
